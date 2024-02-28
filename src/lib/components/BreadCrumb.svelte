17ffbd365906c05f8fd0f3f790f58b726d90f463
JuicyJah <17132956+JuicyJah@users.noreply.github.com>
2024-02-28 13:39:06 -0500

    add project listing page

---
+++ src/lib/components/BreadCrumb.svelte
@@ -0,0 +1,46 @@
+<script>
+	export let crumbs = [
+		{
+			text: 'Home',
+			icon: 'fa-solid fa-house',
+			href: '/'
+		}
+	]
+</script>
+
+<nav class="flex py-4 text-md font-medium" aria-label="Breadcrumb">
+	<ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse text-gray-500 dark:text-gray-400">
+		{#each crumbs as crumb, index}
+			{#if index == 0}
+				<li class="inline-flex items-center">
+					<a href={crumb.href} class="inline-flex items-center text-[inherit] hover:text-indigo-700 dark:hover:text-white">
+						{#if crumb.icon}
+							<i class="mr-2 {crumb.icon}" />
+						{/if}
+						{crumb.text}
+					</a>
+				</li>
+			{:else if index < crumbs.length - 1}
+				<li>
+					<div class="flex items-center">
+						<i class="mr-2 fa-solid fa-chevron-right"></i>
+						<!-- <a href="#" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Projects</a> -->
+						<a href={crumb.href} class="text-gray-700 dark:text-gray-400 text-[inherit] hover:text-indigo-700 dark:hover:text-white">
+							{#if crumb.icon}
+								<i class="mr-2 {crumb.icon}" />
+							{/if}
+							{crumb.text}
+						</a>
+					</div>
+				</li>
+			{:else if index >= crumbs.length - 1}
+				<li aria-current="page">
+					<div class="flex items-center">
+						<i class="fa-solid fa-chevron-right"></i>
+						<span class="ms-1 text-[inherit] md:ms-2">{crumb.text}</span>
+					</div>
+				</li>
+			{/if}
+		{/each}
+	</ol>
+</nav>
---
+++ src/routes/projects/Project.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let project
+</script>
+
+<a href="/projects/{project.id}" class="flex flex-col font-color-default block min-w-0 max-w-sm flex-1 rounded-lg shadow p-4 m-2 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700">
+	<h1 class="text-2xl font-bold leading-7 sm:truncate sm:text-3xl sm:tracking-tight">{project.name}</h1>
+	<p class="text-md leading-7 sm:tracking-tight py-4 grow">{project.description}</p>
+
+	<div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap">
+		{#each project?.tags as tag}
+			<div class="mt-2 mr-3 flex items-center text-sm text-gray-500 dark:text-gray-400">
+				{#if tag.icon}
+					<i class="mr-2 {tag.icon}" />
+				{/if}
+				{tag.text}
+			</div>
+		{/each}
+	</div>
+	{#if !!project?.demo}
+		<div class="mt-2 flex items-center text-sm text-gray-500 dark:text-gray-400">
+			<i class="mr-2 fa-regular fa-circle-play" />
+			Demo Available
+		</div>
+	{/if}
+
+	{#if !!project?.github}
+		<div class="mt-2 flex items-center text-sm text-gray-500 dark:text-gray-400">
+			<i class="mr-2 fa-brands fa-github" />
+			Source Code
+		</div>
+	{/if}
+</a>
